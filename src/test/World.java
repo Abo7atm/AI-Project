ffe8e41524b4051ec0b52f03e96edad0a3e9c205
abo7atm <aaalnumay@gmail.com>
2018-04-08 21:16:39 +0300

    some work done

--- src/test/World.java
+++ src/test/World.java
@@ -35,6 +35,83 @@ public class World {
 		
 	}
 
+	public void displayState() {
+//		int man = 0;
+//		int smallWolf = 0;
+//		int bigWolf = 0;
+//		int boat = 0;
+//		
+//		for(int i=0; i<3; i++) {
+//			if(state[i]) man++;
+//		}
+//		
+//		for(int i=4; i<6; i++) {
+//			if (state[i]) smallWolf++;
+//		}
+//		
+//		if (state[3]) bigWolf++;
+//		
+//		if (state[6]) {
+//			System.out.println("There are " + man + "and\n" +
+//					bigWolf + " big wolf and\n" + 
+//					smallWolf + "small wolf and\n" +
+//					"and the boat at the starting side");
+//		}
+//		
+//		else {
+//			System.out.println("There are " + (3-man) + "and\n" +
+//					(1-bigWolf) + " big wolf and\n" + 
+//					(2-smallWolf) + "small wolf on the ");
+//		}
+		
+		/* According to state representation explained above
+		 * if the index of the participant is true
+		 * then it is on the starting side
+		 * if it's false
+		 * then it is on the finishing side.
+		 */
+		
+		System.out.println("Starting Side:");
+		for (int i=0; i<3; i++) {
+			if(state[i]) {
+				System.out.print("M ");
+			}
+		}
+		
+		if(state[3]) System.out.print("W ");
+		
+		for (int i=4; i<6; i++) {
+			if(state[i]) {
+				System.out.print("w ");
+			}
+		}
+		
+		if(state[6]) {
+			System.out.print("B");
+		}
+		System.out.println("");
+		
+		System.out.println("Finshing side:");
+		for (int i=0; i<3; i++) {
+			if(!state[i]) {
+				System.out.print("M ");
+			}
+		}
+		
+		if(!state[3]) System.out.print("W ");
+		
+		for (int i=4; i<6; i++) {
+			if(!state[i]) {
+				System.out.print("w ");
+			}
+		}
+		
+		if(!state[6]) {
+			System.out.print("B");
+		}
+		System.out.println("");
+	}
+	
 	public boolean goalTest() {
 		boolean result=true;
 		for(int i=0; i<7; i++) {
